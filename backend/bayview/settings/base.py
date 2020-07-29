90d09202ea72a7c9955484c2cecfc2fae38c36a2
Naresh Singh <naresh.singh@sourcefuse.com>
2020-07-29 23:47:44 +0530

    remove

--- backend/bayview/settings/base.py
+++ backend/bayview/settings/base.py
@@ -223,8 +223,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'manthan.anejaa@sourcefuse.com'
-EMAIL_HOST_PASSWORD = 'Manthan1@5'
+EMAIL_HOST_USER = '<user-email>'
+EMAIL_HOST_PASSWORD = '<user-password>'
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 # database configurations
